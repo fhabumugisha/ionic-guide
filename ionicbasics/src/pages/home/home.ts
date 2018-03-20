8cd5d1b04ed23537184a1b9c6f67b5759dafd4ab
HABUMUGISHA <fabrice.a.habumugisha@capgemini.com>
2018-03-20 06:32:13 +0100

    test commit subfolder

---
+++ ionicbasics/src/pages/home/home.ts
@@ -0,0 +1,26 @@
+import { UserPage } from './../users/user/user';
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { UsersPage } from '../users/users';
+import { ShopPage } from '../shop/shop';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  userPage = UsersPage;
+  shopPage = ShopPage;
+
+ /*  constructor(public navCtrl: NavController) {
+
+  } */
+
+ /*  onGoToUsers(){
+    this.navCtrl.push(UsersPage);
+  } */
+
+ /*  onGoToShop(){
+    this.navCtrl.push(ShopPage);
+  } */
+}
