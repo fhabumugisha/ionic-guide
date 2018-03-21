7a118949a529fcf001c051f853b36847c3c20d16
HABUMUGISHA <fabrice.a.habumugisha@capgemini.com>
2018-03-21 07:13:03 +0100

    Initialize the Favorites Quotes App

---
+++ quotes-app/src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { TabsPage } from './../pages/tabs/tabs';
+import { LibraryPage } from '../pages/library/library';
+import { FavoritesPage } from './../pages/favorites/favorites';
+import { QuotePage } from './../pages/quote/quote';
+import { QuotesPage } from './../pages/quotes/quotes';
+import { SettingsPage } from './../pages/settings/settings';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    TabsPage,
+    FavoritesPage,
+    LibraryPage,
+    QuotesPage,
+    QuotePage,
+    SettingsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    TabsPage,
+    FavoritesPage,
+    LibraryPage,
+    QuotesPage,
+    QuotePage,
+    SettingsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
